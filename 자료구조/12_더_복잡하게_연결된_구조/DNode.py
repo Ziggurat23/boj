396a3aeeb06a538e07ec3f794bbffe7e02140df3
GoldenDumpling <123299344+GoldenDumpling@users.noreply.github.com>
2024-01-24 23:20:01 +0900

    Delete 자료구조 directory

--- 자료구조/12_더_복잡하게_연결된_구조/DNode.py
+++
@@ -1,6 +0,0 @@
-
-class DNode:
-    def __init__(self,elem,prev=None,next=None):
-        self.data= elem
-        self.prev= prev
-        self.next= next
--- 자료구조/12_더_복잡하게_연결된_구조/Node.py
+++
@@ -1,5 +0,0 @@
-
-class Node:
-    def __init__(self,elem,link):
-        self.data= elem
-        self.link= link
