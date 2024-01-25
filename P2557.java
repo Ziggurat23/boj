4313367332080da7b1e56bbf87c30830d3ae2b20
GoldenDumpling <123299344+GoldenDumpling@users.noreply.github.com>
2024-01-25 09:13:12 +0900

    Update and rename HelloWorld.java to P2557.java

--- P2557.java
+++ P2557.java
@@ -5,6 +5,7 @@ import java.io.OutputStreamWriter;
 import java.io.IOException;
  
 public class HelloWorld {
+//Hello World
 	public static void main(String[] args) throws IOException {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
  
